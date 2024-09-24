c048724dc17c7ce5add799f8314a44377c3e1908
Jeff John <John-J8@ulster.ac.uk>
2024-09-24 14:46:22 +0100

    Made a Basic Calculator App.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This integer variable stores the first number. 
+// Ask the user to input the first number. 
+Console.WriteLine("Type in the first number followed by the Enter key");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// This integer variable stores the second number. 
+// Ask the user to input the second number. 
+Console.WriteLine("Type in the second number followed by the Enter key");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// Perform the caluculation
+int result = firstNumber + secondNumber;
+
+// Output the result
+Console.WriteLine("Adding {0} and {1} gives the answer {2}", firstNumber, secondNumber, result);
+
